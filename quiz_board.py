29adc066c9b17ae99fac3c80cecd24b72abf8925
Arishneel Narayan <aarissagar@gmail.com>
2025-09-04 19:23:51 +1200

    asdfzC

--- quiz_board.py
+++ quiz_board.py
@@ -3,14 +3,21 @@ import streamlit as st
 st.set_page_config(page_title="Quiz Board", layout="centered")
 
 
-# Let user choose number of questions
-num_questions = st.number_input("Number of Questions", min_value=1, max_value=30, value=9, step=1, key="num_questions")
-
-# Initialize session state for questions
-if 'questions' not in st.session_state or len(st.session_state.questions) != num_questions:
-    st.session_state.questions = [False] * num_questions
+# Only show number input if not set yet
+if 'num_questions' not in st.session_state:
+    num_questions = st.number_input("Number of Questions", min_value=1, max_value=30, value=9, step=1, key="num_questions_input")
+    if st.button("Set Number of Questions"):
+        st.session_state.num_questions = num_questions
+        st.session_state.questions = [False] * num_questions
+        st.experimental_rerun()
+    st.stop()
+else:
+    num_questions = st.session_state.num_questions
+    if 'questions' not in st.session_state or len(st.session_state.questions) != num_questions:
+        st.session_state.questions = [False] * num_questions
 
 st.title("Quiz Question Board")
+
 
 # Display questions in a 3x3 grid (or more if needed)
 cols = st.columns(3)
@@ -25,4 +32,6 @@ for i in range(num_questions):
                 st.session_state.questions[i] = True
 
 st.markdown("---")
-
+if st.button("Reset Board", use_container_width=True):
+    st.session_state.questions = [False] * 9
+    st.experimental_rerun()
